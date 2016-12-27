3800c1d65116edc26ba6b37edf37d28e4074708c
SegFault0x0 <SegFault0x0@users.noreply.github.com>
2016-12-26 23:28:22 -0500

    Add list of markers and get it to control marker click events.

--- js/map.js
+++ js/map.js
@@ -1,15 +1,74 @@
 'use strict';
 
 var wikiElem;
+var infoWindow;
 
+/*** MODEL DATA ***/
+var markers = ko.observableArray([]);
 var locations = [
-    {title: 'Park Avenue', location: {lat: 40.7713024, lng: -73.9632393}},
-    {title: 'Starbucks', location: {lat: 40.7444883, lng: -73.9949465}},
-    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    {
+        title: 'Park Avenue',
+        location: {
+            lat: 40.7713024,
+            lng: -73.9632393
+        },
+    },
+    {
+        title: 'Starbucks',
+        location: {
+            lat: 40.7444883,
+            lng: -73.9949465
+        },
+    },
+    {
+        title: 'Union Square Open Floor Plan',
+        location: {
+            lat: 40.7347062,
+            lng: -73.9895759
+        },
+    },
+    {
+        title: 'East Village Hip Studio',
+        location: {
+            lat: 40.7281777,
+            lng: -73.984377
+        },
+    },
+    {
+        title: 'TriBeCa Artsy Bachelor Pad',
+        location: {
+            lat: 40.7195264,
+            lng: -74.0089934
+        },
+    },
 ];
+
+/**
+ * Serves as KnockoutJS's `Controller`.
+ */
+var ViewModel = function() {
+    /**
+     * Use `self` to access the ViewModel when the keyword `this` is referencing
+     * an inner function.
+     */
+    var self = this;
+
+    // Create the array of locations
+    // this.places = ko.observableArray([]);
+    this.places = markers;
+
+    // Keep track of the selected marker
+    this.currentMarker = ko.observable(this.places()[0]);
+
+    // Create function to engage the marker
+    this.selectMarker = function(clickedMarker) {
+        self.currentMarker(clickedMarker);
+        toggleBouncing(self.currentMarker());
+        populateInfoWindow(self.currentMarker(), infoWindow);
+        console.log('current Marker: ' + self.currentMarker().title);
+    };
+};
+
 
 /**
  * Retrieves a tidbit of general information about a location from Wikipedia.
@@ -32,13 +91,34 @@ var getWikiData = function(place) {
         var wikiInfo = response[2][0];
         console.log(wikiInfo);
 
-        clearTimeout(wikiTmeout);
-
         return (wikiInfo);
     }).fail((err) => {
         console.log(err);
         return ('No Wikipedia data available.')
     });
+};
+
+/**
+ * Populates data for an InfoWindow.
+ * @param {google.maps.Marker} marker
+ * @param {google.maps.InfoWindow} markerWindow
+ */
+var populateInfoWindow = function(marker, markerWindow) {
+    // Don't open another markerWindow if one is already opened.
+    if (markerWindow.marker != marker) {
+        markerWindow.marker = marker;
+        markerWindow.setContent(
+            '<div>' + marker.title + '</div>' +
+            '<div>' + '<hr>' + '</div>' +
+            '<div>' + marker.data + '</div>'
+        );
+        markerWindow.open(map, marker);
+
+        // NULL the marker property upon close
+        markerWindow.addListener('closeclick', () => {
+            markerWindow.marker = null;
+        });
+    }
 };
 
 /**
@@ -52,32 +132,11 @@ var toggleBouncing = function(marker) {
     }
 };
 
-
 /**
  * Creates the map, markers, and all basic map functionality
  */
 var initMap = function() {
     var marker;
-
-    // Function that populates data for an InfoWindow
-    var populateInfoWindow = function(marker, markerWindow) {
-        // Don't open another markerWindow if one is already opened.
-        if (markerWindow.marker != marker) {
-            markerWindow.marker = marker;
-            markerWindow.setContent(
-                '<div>' + marker.title + '</div>' +
-                '<div>' + '<hr>' + '</div>' +
-                '<div>' + marker.data + '</div>'
-            );
-            markerWindow.open(map, marker);
-
-            // NULL the marker property upon close
-            markerWindow.addListener('closeclick', () => {
-                markerWindow.marker = null;
-            });
-        }
-    };
-
 
     // Create the map
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -88,11 +147,11 @@ var initMap = function() {
         zoom: 13
     });
 
-
-    // Populate the map with markers for Points Of Interest
-    var infoWindow = new google.maps.InfoWindow();
+    // Populate the map with markers for Points of Interest
+    infoWindow = new google.maps.InfoWindow();
     var latLon = {};
     var title = '';
+    var marker = {};
 
     for (var i = 0, len = locations.length; i < len; ++i) {
         latLon = locations[i].location;
@@ -112,7 +171,12 @@ var initMap = function() {
             //FIXME: Doesn't bounce until secondary click.
             toggleBouncing(this);
         });
+
+        // Add marker to Observable markers array
+        markers.push(marker);
     }
 
 };
 
+// Activate KnockoutJS
+ko.applyBindings(new ViewModel());
